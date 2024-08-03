22210b8d3a595992e3de5ae58803f38c827121a2
Shanmuka Srinivas <shanmuka srinivasa>
2024-08-03 15:41:00 +0530

    second bug fix

--- src/pages/seller/pages/ShowProducts.jsx
+++ src/pages/seller/pages/ShowProducts.jsx
@@ -9,6 +9,7 @@ import AddCardIcon from '@mui/icons-material/AddCard';
 import DeleteIcon from "@mui/icons-material/Delete";
 import UploadIcon from '@mui/icons-material/Upload';
 import AlertDialogSlide from '../../../components/AlertDialogSlide';
+import { Grid,Box } from '@mui/material';
 
 const ShowProducts = () => {
   const dispatch = useDispatch();
